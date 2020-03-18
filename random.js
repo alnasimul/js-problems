09d6c3f04a66487ddff7e513187f7ad7edc4a3c7
alnasimul <alnasimul@gmail.com>
2020-03-18 08:48:41 +0600

    All about random number

---
+++ random.js
@@ -0,0 +1,14 @@
+var num = 2.1925;
+
+var result1 = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+
+var randomNum = 1+Math.random()*5;
+
+var dice = Math.round(randomNum);
+
+//console.log(result1);
+// console.log(result2);
+// console.log(result3);
+console.log(dice);
